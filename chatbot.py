3357f158ae5ff2eec9603fa4fad5515e939758cd
manyu <manyu@manyus-MacBook-Pro.local>
2019-10-16 14:50:26 +0530

    first push of d NLP

---
+++ chatbot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 13:29:15 2019
+
+@author: manyu
+"""
+
+import numpy as np
+import tensorflow as tf
+import re
+import time
+
