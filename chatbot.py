7adad2cf674e21db42d353a58407e41a617ed217
manyu <manyu@manyus-MacBook-Pro.local>
2019-10-16 16:57:36 +0530

    data processing

--- chatbot.py
+++ chatbot.py
@@ -11,3 +11,21 @@ import tensorflow as tf
 import re
 import time
 
+print("hello")
+
+lines=open("movie_lines.txt", encoding="utf-8", errors="ignore").read().split("\n")
+movie_convesation=open("movie_conversations.txt", encoding="utf-8", errors="ignore").read().split("\n")
+
+id2line={}
+
+###
+for line in lines:
+   _line=line.split(' +++$+++ ')
+   if len(_line)==5:
+       id2line[_line[0]]=_line[4]
+       
+
+conversation_ids={}
+for conversation in movie_convesation[:-1]:
+    _conversation = movie_convesation.split(' +++$+++ ')[-1][1:-1].replace("'","").replace(" ","") 
+    conversation_ids.append[_conversation.split(',')]      
